5757dbf4c0e462305e59ba00a6a5159fe3882100
Bushra Javed <39137046+bushrajav@users.noreply.github.com>
2019-01-29 15:26:15 +0000

    Add files via upload

---
+++ ReactPractice/build/ReactCode.jsx
@@ -0,0 +1,11 @@
+//ReactDOM.render(<h1>bushra</h1>,document.getElementById('example'));
+var MyComponent =React.createClass({
+	render :function(){
+		return (
+		<h2>hello</h2>
+		);
+	}
+	
+});
+ReactDOM.render(
+<MyComponent/>,document.getElementById('example'));
